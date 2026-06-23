68376fcf9d35120c56f1e97c96ce848b7d2cffd0
unknown <joaoguilhermpradolopes@gmail.com>
2026-06-23 16:21:37 -0300

    first commit

---
+++ projeto-lanchonete/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import Login from "./components/Login"
+
+function App(){
+  return (
+    <>
+    <Header titulo="Lanchonete Dogão"/>
+    <Header subtitulo="O melhor da refião"/>
+    <Login />
+    </>
+  )
+}
+
+export default App
